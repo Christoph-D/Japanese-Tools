8945672189b93c7924fc088fc21d828c184503a5
Christoph Dittmann <github@christoph-d.de>
2010-05-09 22:18:05 +0200

    Remove comments from kanjidic when preparing the file because the parser doesn't handle them.

--- kanjidic/prepare_kanjidic.sh
+++ kanjidic/prepare_kanjidic.sh
@@ -19,6 +19,10 @@ echo 'Encoding kanjidic to utf-8...'
 iconv -f EUCJP -t UTF-8 kanjidic > kanjidic_utf8
 mv kanjidic_utf8 kanjidic
 
+echo 'Removing comments...'
+grep -v '^#' kanjidic > kanjidic_no_comments
+mv kanjidic_no_comments kanjidic
+
 echo 'Done.'
 
 exit 0
