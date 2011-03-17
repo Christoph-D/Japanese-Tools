d851f00367ef5a698bea6fe65899de1875cb0cfe
Christoph Dittmann <github@christoph-d.de>
2011-03-17 19:46:37 +0100

    Add support to plural forms in gettext.sh.

--- gettext/gettext.sh
+++ gettext/gettext.sh
@@ -17,8 +17,22 @@ printf_() {
     shift
     printf "$(gettext "$FORMAT")\n" "$@"
 }
+nprintf_() {
+    local SINGULAR="$1"
+    local PLURAL="$2"
+    local N="$3"
+    shift 3
+    printf "$(ngettext "$SINGULAR" "$PLURAL" "$N")\n" "$@"
+}
 printf_no_newline_() {
     local FORMAT="$1"
     shift
     printf "$(gettext "$FORMAT")" "$@"
+}
+nprintf_no_newline_() {
+    local SINGULAR="$1"
+    local PLURAL="$2"
+    local N="$3"
+    shift 3
+    printf "$(ngettext "$SINGULAR" "$PLURAL" "$N")" "$@"
 }
