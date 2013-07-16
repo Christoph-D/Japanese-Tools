e71c33ecdc5181a65ea3ffc51bb4bde0dc943d4d
Christoph Dittmann <github@christoph-d.de>
2013-07-16 20:20:56 +0200

    Save skipped words

--- reading_quiz/create_vocab_list.sh
+++ reading_quiz/create_vocab_list.sh
@@ -8,10 +8,18 @@ JA="$(dirname "$0")"/lookup_one_line.sh
 
 INPUT="$1"
 
+SKIP_FILE=skipped.txt
+
 if [[ ! $INPUT ]]; then
     echo 'Usage: create_vocab_list.sh word_list.txt > vocabulary/list.txt'
     echo 'word_list.txt should contain one word per line, in kanji.'
     exit 0
+fi
+
+if [[ -s $SKIP_FILE ]]; then
+    echo "$SKIP_FILE exists and is not empty.  Please delete it." >&2
+    echo 'This file is required to store the skipped words.' >&2
+    exit 1
 fi
 
 TOTAL=$(wc -l "$INPUT")
@@ -22,6 +30,7 @@ while read I; do
     FULL_LINE="$("$JA" "$I" | head -n 1)"
     if [[ $FULL_LINE = 'Unknown word.' ]] ||
         echo "$FULL_LINE" | grep -q '^\([^|]*\)|\1'; then
+        echo "$I" >> "$SKIP_FILE"
         let SKIPPED=$SKIPPED+1
     else
         echo "$FULL_LINE"
