3a2ce0c25bc39b0b78115090781acf6a94f8ad5b
Christoph Dittmann <github@christoph-d.de>
2013-09-15 16:48:27 +0200

    Fix gd.sh

--- google_dictionary/gd.sh
+++ google_dictionary/gd.sh
@@ -5,7 +5,7 @@
 # This script performs a Google dictionary lookup for english words.
 #
 
-URL='http://www.google.com/search?hl=en&tbs=dfn%3A1&cad=h&q='
+URL='http://www.google.com/search?hl=en&q=define+'
 
 MAX_RESULT_LENGTH=300
 
@@ -32,13 +32,57 @@ encode_query() {
     echo "$URL$ENCODED_QUERY"
 }
 ask_google() {
-    RESULT=$(wget --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:5.0) Gecko/20100101 Firefox/5.0' "$(encode_query "$1")" --quiet -O - \
-        | sed 's/<li style="list-style:decimal">\(\([^<]\|<[^d]\|<d[^i]\)*\)</（\1）/g;t;d' \
-        | sed 's#>&emsp;\(/\([^/]\|/[^&]\)*/\)&emsp;<#（\1）#g;s/&emsp;//g' \
-        | sed 's/^[^（]*（//;s/）[^）]*$//;s/）[^（]*（/.\n/g' \
-        | sed 's#/\.#/#g' \
-        | sed 's#</\?em>#*#g' \
-        | sed 's/<[^>]*>//g' \
+    RESULT=$(wget --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:5.0) Gecko/20100101 Firefox/5.0' \
+        --quiet -O - \
+        "$(encode_query "$1")" \
+        | grep 'class="vk_ans"' \
+        | perl -pe '
+# Find the start of the definition and remove the hyphenated word in
+# the result.
+s#.*?<div class="vk_ans".*?>.*?</div>(.*)#$1#g;
+
+# Replace <i>foo</i> with *foo*.
+s#</?i>#*#g;
+
+s#<script.*?(</script>|$)##g;
+
+# Find the pronunciation and mark it with //.
+s#.*<span class="lr_dct_ph"><span>([^<]*)#/$1/#;
+
+# Remove the word class ("adjective", "noun", etc.).
+s#<div class="lr_dct_sf_h">.*?</div>##;
+s#<div class="xpdxpnd vk_gy".*?</div>##g;
+
+# Remove all definitions except the first one.
+s#<div class="lr_dct_sf_h">.*##;
+
+# Remove example sentences.
+s#<div class="vk_gy">.*?</div>##g;
+
+# Remove synonyms and antonyms.
+s#<table class="vk_tbl vk_gy">.*?</table>##g;
+
+# Remove etymology.
+s#<div class="xpdxpnd".*##;
+
+# Surround topic markers with parentheses.  Example: (Music) in the
+# definition of "space".
+s#<span class="lr_dct_lbl_blk.*?>(.*?)</span>#($1)#g;
+
+# Sanity check: The result should begin with a pronunciation or a
+# <div> tag without attributes.  If it begins with something else,
+# something is wrong and we better drop everything.
+s#^(?!(/|<div>)).*##;
+
+# Remove all remaining tags
+s/<.*?>/ /g;
+
+# Collapse spaces.
+s# / ##; s# +# #g; s#^ ##; s/ $//; s# ([,.])#$1#g;
+
+# Remove a single dot at the end.
+s/\.$//;' \
+        | sed '/^[^2]*$/s/1\. //' \
         | fix_html_entities \
         | fix_html_entities)
     if [[ -n $RESULT ]]; then
