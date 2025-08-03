c7ea8f2abb64308bb1a67e73274d1753b187f7ca
Christoph Dittmann <github@yozora.eu>
2025-08-03 18:02:46 +0200

    Reading: Convert to Python 3

--- reading/read.py
+++ reading/read.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
@@ -9,8 +9,12 @@
 # Adapted for stand-alone use by
 # Christoph Dittmann <github@christoph-d.de>.
 
-import sys, os, platform, re, subprocess, codecs
-
+import sys
+import os
+import platform
+import re
+import subprocess
+import codecs
 MAX_OUTPUT_LENGTH = 300
 
 kakasiCmd = ["kakasi", "-iutf8", "-outf8", "-u", "-JH", "-KH"]
@@ -32,22 +36,22 @@ class KakasiController(object):
 
     def toHiragana(self, expr):
         self.ensureOpen()
-        self.kakasi.stdin.write(expr.encode("utf8", "ignore")+'\n')
+        self.kakasi.stdin.write(expr.encode("utf8", "ignore") + b'\n')
         self.kakasi.stdin.flush()
-        res = unicode(self.kakasi.stdout.readline().rstrip('\r\n'), "utf8")
+        res = self.kakasi.stdout.readline().rstrip(b'\r\n').decode("utf8")
         return res
 
 kakasi = KakasiController()
 
 def fixExpr(expr):
     out = []
-    expr_split = re.split("([^\[]+\[[^\]]*\])", expr)
+    expr_split = re.split("([^\\[]+\\[[^\\]]*\\])", expr)
     for node in expr_split:
         if node == '':
             continue
-        m = re.match("(.+)\[(.*)\]", node.decode("utf-8"))
+        m = re.match("(.+)\\[(.*)\\]", node)
         if not m:
-            out.append(node.decode("utf-8"))
+            out.append(node)
             continue
         (kanji, reading) = m.groups()
         # hiragana, katakana, punctuation, not japanese, or lacking a reading
@@ -61,21 +65,21 @@ def fixExpr(expr):
             out.append(kanji)
             continue
         # don't add readings of numbers
-        if kanji.strip() in u"０１２３４５６７８９": # u"一二三四五六七八九十０１２３４５６７８９":
+        if kanji.strip() in "０１２３４５６７８９": # "一二三四五六七八九十０１２３４５６７８９":
             out.append(kanji)
             continue
         # strip matching characters and beginning and end of reading and kanji
         # reading should always be at least as long as the kanji
         placeL = 0
         placeR = 0
-        for i in range(1,len(kanji)):
+        for i in range(1, len(kanji)):
             if kanji[-i] != reading[-i]:
                 break
             placeR = i
-        for i in range(0,len(kanji)-1):
+        for i in range(0, len(kanji) - 1):
             if kanji[i] != reading[i]:
                 break
-            placeL = i+1
+            placeL = i + 1
         if placeL == 0:
             if placeR == 0:
                 out.append(" %s[%s]" % (kanji, reading))
@@ -92,11 +96,11 @@ def fixExpr(expr):
                     reading[placeL:-placeR], reading[-placeR:]))
     fin = ""
     for c, s in enumerate(out):
-        if c < len(out) - 1 and re.match("^[A-Za-z0-9]+$", out[c+1]):
+        if c < len(out) - 1 and re.match("^[A-Za-z0-9]+$", out[c + 1]):
             s += " "
         fin += s
     fin = fin.strip()
-    fin = re.sub(u"\[\]", u"", fin)
+    fin = re.sub(u"\\[\\]", u"", fin)
     fin = re.sub(u" +", u" ", fin)
     return fin
 
@@ -105,22 +109,22 @@ def get_readings(expr):
         mecab = subprocess.Popen(
             mecabCmd, bufsize=-1, stdin=subprocess.PIPE,
             stdout=subprocess.PIPE)
-        return mecab.communicate(expr)[0]
+        return mecab.communicate(expr.encode('utf-8'))[0].decode('utf-8')
     except OSError:
         raise Exception("Please install mecab.")
 
 if __name__ == "__main__":
     sys.stdout = codecs.open("/dev/stdout", "w", 'utf-8')
     if len(sys.argv) != 2 or len(sys.argv[1]) == 0:
-        print 'Please provide one argument.'
+        print('Please provide one argument.')
         sys.exit(0)
     try:
         result = fixExpr(get_readings(sys.argv[1]))
         result = re.sub(u"\\n", u"", result)
-    except Exception, (e):
-        print e
+    except Exception as e:
+        print(e)
         sys.exit(1)
     if len(result) > MAX_OUTPUT_LENGTH:
-        print result[0:MAX_OUTPUT_LENGTH - 3] + u'...'
+        print(result[0:MAX_OUTPUT_LENGTH - 3] + u'...')
     else:
-        print result
+        print(result)
