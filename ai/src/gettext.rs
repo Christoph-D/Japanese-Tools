4ea6c2af845513e45946ddbec880b9f6d2d0e26b
Christoph Dittmann <github@yozora.eu>
2025-08-04 00:12:35 +0200

    ai: Fully qualify gettext in the formatget! macro

--- ai/src/gettext.rs
+++ ai/src/gettext.rs
@@ -3,6 +3,6 @@
 macro_rules! formatget {
     ($fmt:expr $(, $args:expr)* $(,)?) => {{
         let fmt_str: &str = $fmt.as_ref();
-        formatx::formatx!(gettext(fmt_str) $(, $args)*).unwrap_or($fmt.to_string())
+        formatx::formatx!(gettextrs::gettext(fmt_str) $(, $args)*).unwrap_or($fmt.to_string())
     }};
 }
